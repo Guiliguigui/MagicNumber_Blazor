fb2de53f0e0d76049fcc2db9a610371ccb7a5968
Guiliguigui <gui.mairesse@gmail.com>
2022-04-25 16:38:13 +0200

    Ajoutez des fichiers projet.

---
+++ Pages/MagicNumber.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorApp1.Pages
+{
+    public class MagicNumberBase : ComponentBase
+    {
+        protected const int max = 20;
+        protected int MagicNumber { get; set; }
+        protected int Number { get; set; }
+        protected int Lifes { get; set; } = 5;
+        protected string LifesString => new string('❤', Lifes);
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+        protected MagicNumberBase()
+        {
+            Random rand = new Random();
+            MagicNumber = rand.Next(max + 1);
+        }
+
+        protected void TestNumber()
+        {
+            if (MagicNumber == Number)
+            {
+                NavigationManager.NavigateTo("/MagicNumber/Result/true");
+                return;
+            }
+            Lifes--;
+            if (Lifes == 0)
+            {
+                NavigationManager.NavigateTo("/MagicNumber/Result/false");
+            }
+        }
+    }
+}
